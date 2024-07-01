4e4f2d79af7cd178ebb7f6c52d1f7cb0922ef871
meshkatshb <meshkat97@yahoo.com>
2024-07-01 18:32:50 +0330

    feat(detection): Add jaeger API port and domain address with services names.

--- detection/utils.py
+++ detection/utils.py
@@ -2,9 +2,22 @@ BASE_PATH = 'H:/Documents/Apply/Applications/TU Delft/Assessment/test-assignment
 TRACES = '/trace_exploration/traces'
 
 JAEGER_LOG_PATH_ADD_CAT = BASE_PATH + TRACES + '/trace_add_cat.json'
+
 JAEGER_LOG_PATH_FIND_CAT = BASE_PATH + TRACES + '/trace_find_cats_by_name.json'
+
 JAEGER_LOG_PATH_PAIRS = BASE_PATH + TRACES + '/trace_generate_pairs.json'
+
 JAEGER_LOG_PATH_WITH_ERROR = BASE_PATH + TRACES + '/trace_generate_pairs_with_error.json'
+
 JAEGER_LOG_PATH_GET_ALL_CATS = BASE_PATH + TRACES + '/trace_get_all_cats.json'
 
 TRACE_DURATION_THRESHOLD = 1000
+
+JAEGER_PORT = '16686'
+JAEGER_DOMAIN = 'localhost'
+JAEGER_API_URL = 'http://' + JAEGER_DOMAIN + ':' + JAEGER_PORT + '/api/traces'
+
+SERVICES = [
+    "cat-api",
+    "cat-recommender-api",
+]
